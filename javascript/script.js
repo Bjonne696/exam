650b50cf25fba7aaf44308daee3c1ada096eea67
Bjorn_Tore <Bjonne696@gmail.com>
2022-12-27 17:03:53 +0100

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -9,6 +9,7 @@ const options = {
  fetch('https://tasty.p.rapidapi.com/recipes/list?from=0&size=40&tags=under_30_minutes', options)
 	.then(response => response.json())
 	.then(response => console.log(response))
+    .catch(err => console.error(err)); 
 
     
    /*
